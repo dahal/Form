10a17e6cf3ceceb0f1aaff6391794b772cea0931
Puru Dahal <pdahal@me.com>
2015-03-01 01:27:54 -0800

    Add user authentication, get some design done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,4 +73,14 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  ### DEVISE
+  # Ensure you have defined default url options in your environments files. Here
+  # is an example of default_url_options appropriate for a development environment
+  # in config/environments/development.rb:
+
+  #   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # In production, :host should be set to the actual host of your application.
+  config.action_mailer.default_url_options = { host: 'fom.io' }
 end
