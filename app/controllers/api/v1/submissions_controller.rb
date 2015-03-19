a9b883fa7968e3076bae52db4cc5666a2618bca4
Puru Dahal <pdahal@me.com>
2015-03-19 01:10:53 -0700

    Complete API V1

--- app/controllers/api/v1/submissions_controller.rb
+++ app/controllers/api/v1/submissions_controller.rb
@@ -2,39 +2,27 @@ class Api::V1::SubmissionsController < ApplicationController
   skip_before_filter :verify_authenticity_token
   before_action :set_form
   before_action :is_not_spam?, only: [:create]
+  respond_to :json
 
   def index
-    @submissions = Submission.where(form_id: @form.id).page(params[:page])
-    render json: @submissions, meta: {
-      current_page: @submissions.current_page,
-      next_page: @submissions.next_page,
-      prev_page: @submissions.prev_page,
-      total_pages: @submissions.total_pages,
-      total_count: @submissions.total_count
+    submissions = Submission.where(form_id: @form.id).page(params[:page])
+
+    meta = {
+      meta: {
+        form: @form.form_id,
+        current_page: submissions.current_page,
+        next_page: submissions.next_page,
+        prev_page: submissions.prev_page,
+        total_pages: submissions.total_pages,
+        total_count: submissions.total_count
+      }
     }
-  end
 
-  def create
-    if is_not_spam?
-      @submission = Submission.create(
-          form_id: @form.id,
-          details: submission_params
-        )
-    end
-
-    redirect_to params[:redirect] || 'http://example.com'
+    respond_with submissions, meta
   end
 
   private
   def set_form
     @form = Form.find_by(form_id: params[:form_id])
-  end
-
-  def is_not_spam?
-    params[:utf8] == 'Î²'
-  end
-
-  def submission_params
-    params.except(:controller, :action, :utf8, :submit)
   end
 end
--- app/controllers/dashboard/submissions_controller.rb
+++ app/controllers/dashboard/submissions_controller.rb
@@ -1,3 +1,25 @@
 class Dashboard::SubmissionsController < Dashboard::BaseController
+  def create
+    if is_not_spam?
+      @submission = Submission.create(
+          form_id: @form.id,
+          details: submission_params
+        )
+    end
 
+    redirect_to params[:redirect] || 'http://fom.io/thanks'
+  end
+
+  private
+  def set_form
+    @form = Form.find_by(form_id: params[:form_id])
+  end
+
+  def is_not_spam?
+    params[:utf8] == 'Î²'
+  end
+
+  def submission_params
+    params.except(:controller, :action, :utf8, :submit)
+  end
 end
